aecfd71e5966b91a2def3adfdc629a80dc227b9e
Your Name <duydong19032002@gmail.com>
2024-09-12 10:42:45 +0700

    first commit

---
+++ tuan4/src/App.js
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+
+function App() {
+  const [counter,setCounter] = useState(0)
+    const handleIncrement = () => setCounter(counter + 1)
+  return (
+    <div className="App" style={{padding:20}}>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrement}>click</button>
+    </div>
+  );
+}
+
+export default App;
